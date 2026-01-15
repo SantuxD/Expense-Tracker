f6774d4d807757d7889d3cc0ffe3649f838a3d7f
Santu Mondal <mondalsantu104@gmail.com>
2026-01-16 00:13:55 +0530

    create income routes

---
+++ backend/controller/income.controller.js
@@ -0,0 +1,20 @@
+const user = require("../models/User.models");
+const Income = require("../models/Income.models");
+
+
+const addIncome = async (req, res) => {
+
+};
+
+const getAllIncome = async (req, res) => {};
+
+const deleteIncome = async (req, res) => {};
+
+const downloadIncomeExcel = async (req, res) => {};
+
+module.exports = {
+  addIncome,
+  getAllIncome,
+  deleteIncome,
+  downloadIncomeExcel,
+};
