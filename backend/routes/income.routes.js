18fefd4f570c63ed50ef40aa72418412eb3967c1
Santu Mondal <mondalsantu104@gmail.com>
2026-01-17 21:57:56 +0530

    initialize income route

---
+++ backend/routes/income.routes.js
@@ -0,0 +1,16 @@
+const express = require("express")
+const { addIncome, getAllIncome, deleteIncome, downloadIncomeExcel } = require("../controller/income.controller")
+
+const {protect } = require("../middleware/auth.middleware")
+
+const router = express.Router();
+
+router.post("/add", protect, addIncome)
+router.get("/get", protect, getAllIncome)
+router.delete("/:id", protect, deleteIncome)
+router.post("/add", protect, downloadIncomeExcel)
+
+
+
+module.exports = router;
+
