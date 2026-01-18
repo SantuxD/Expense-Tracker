22b9748ad482db699dbc325185e4fc8fdf5f9ae2
Santu Mondal <mondalsantu104@gmail.com>
2026-01-18 22:09:02 +0530

    [feat add like add income]

--- backend/controller/income.controller.js
+++ backend/controller/income.controller.js
@@ -3,14 +3,39 @@ const Income = require("../models/Income.models");
 
 
 const addIncome = async (req, res) => {
+  const userId = req.user.id;
+  try {
+    const { icon, source, amount, date } = req.body
 
-};
+    if (!source || !amount || !date) {
+      return res.status(400).json({
+        message: "all fields are required"
+      })
+    }
 
-const getAllIncome = async (req, res) => {};
+    const newIncome = new Income({
+      userId,
+      icon,
+      source,
+      amount,
+      date: new Date(date)
 
-const deleteIncome = async (req, res) => {};
+    })
 
-const downloadIncomeExcel = async (req, res) => {};
+    await newIncome.save();
+    console.log(req.body);
+    res.status(200).json(newIncome)
+  } catch (error) {
+    res.status(500).json({ message: "server Error" + error.message })
+  }
+
+}
+
+const getAllIncome = async (req, res) => { };
+
+const deleteIncome = async (req, res) => { };
+
+const downloadIncomeExcel = async (req, res) => { };
 
 module.exports = {
   addIncome,
