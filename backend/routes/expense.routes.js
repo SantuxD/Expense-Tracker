5a2d0c6df8fbffdccd070ee05131f6cff726c2d3
Santu Mondal <mondalsantu104@gmail.com>
2026-01-19 21:40:09 +0530

    Create Expense Route

--- backend/routes/expense.routes.js
+++ backend/routes/expense.routes.js
@@ -1,6 +1,11 @@
 const express = require('express');
+const { addExpense, getExpenses, updateExpense, deleteExpense } = require('../controllers/expense.controller');
+const { protect } = require('../middleware/auth.middleware');
 
 
 const  router  = express.Router();
 
-router.use
+router.post("/add", protect, addExpense);
+router.get("/get", protect, getExpenses);
+router.put("/update/:id", protect, updateExpense);
+router.delete("/delete/:id", protect, deleteExpense);
