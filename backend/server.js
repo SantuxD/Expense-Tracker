fe2bb246b0b3dd062958bd2998cc2820e074224a
Santu Mondal <mondalsantu104@gmail.com>
2026-01-23 22:34:34 +0530

    Backend ["Modify port number"]

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,7 @@ const app = express();
 app.use(
   cors(
     (corsOptions = {
-      origin: process.env.CORS_ORIGIN || "http://localhost:3000",
+      origin: process.env.CORS_ORIGIN || "http://localhost:5173",
       methods: ["GET", "POST", "PUT", "DELETE"],
       allowedHeaders: ["Content-Type", "Authorization"],
       credentials: true,
