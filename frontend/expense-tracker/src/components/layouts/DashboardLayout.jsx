15b2b40deb1d73ec1f78ebbf5ac5eb95db7fe5bb
Santu Mondal <mondalsantu104@gmail.com>
2026-01-24 22:09:25 +0530

    Create dashboardLayout UI

---
+++ frontend/expense-tracker/src/components/layouts/DashboardLayout.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import Navbar from "./Navbar";
+import SideMenu from "./SideMenu";
+import { UserContext } from "../../context/Usercontext";
+
+const DashboardLayout = ({children, activeMenu}) => {
+
+    const { user } = React.useContext(UserContext);
+  return (
+    <div>
+      <Navbar activeMenu={activeMenu} />
+
+      {user && (
+        <div className="flex">
+          <div className="max-[1080px]:hidden">
+            <SideMenu activeMenu={activeMenu} />
+          </div>
+          <div className="grow mx-1.5">{children}</div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default DashboardLayout;
