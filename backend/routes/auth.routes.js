567bc15fbc7185d2760d24233ed9d1991fca120f
Santu Mondal <mondalsantu104@gmail.com>
2026-01-24 22:07:29 +0530

    "Seperate auth routes from here"

--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const { protect } = require("../middleware/auth.middleware");
-const  upload  = require("../middleware/upload.middleware");
 
 const router = express.Router();
 
@@ -14,23 +13,5 @@ router.post("/register", registerUser);
 router.post("/login", loginUser);
 router.get("/getUser", protect, getUserInfo);
 
-
-router.post("/uploadProfileImage", upload.single("image"), async (req, res) => {
-  try {
-    if (!req.file) {
-      return res.status(400).json({ message: "No file uploaded" });
-    }
-    const profileImageUrl = `${req.protocol}://${req.get("host")}/uploads/${
-      req.file.filename
-    }`;
-    return res
-      .status(200)
-      .json({ message: "File uploaded successfully", profileImageUrl });
-  } catch (error) {
-    return res
-      .status(500)
-      .json({ message: "Error uploading file: " + error.message });
-  }
-});
 
 module.exports = router;
