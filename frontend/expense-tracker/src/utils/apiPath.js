fd26b6d873592f5545ade448e5e695bc75a7aa44
Santu Mondal <mondalsantu104@gmail.com>
2026-01-24 22:14:30 +0530

    "Modify apipath for reliable connection"

--- frontend/expense-tracker/src/utils/apiPath.js
+++ frontend/expense-tracker/src/utils/apiPath.js
@@ -2,26 +2,26 @@ export const BASE_URL = "http://localhost:8000";
 
 export const API_PATHS = {
   AUTH: {
-    LOGIN: "/api/V1/auth/login",
-    REGISTER: "/api/V1/auth/register",
-    GET_USER_INFO: "/api/V1/auth/getUser",
+    LOGIN: "/api/v1/auth/login",
+    REGISTER: "/api/v1/auth/register",
+    GET_USER_INFO: "/api/v1/auth/getUser",
   },
   DASHBOARD: {
-    GET_DATA: "/api/V1/dashboard",
+    GET_DATA: "/api/v1/dashboard",
   },
   INCOME: {
-    ADD_INCOME: "/api/V1/income/add",
-    GET_INCOMES: "/api/V1/income/getAll",
-    DELETE_INCOME: (incomeId) => `/api/V1/income/${incomeId}`,
-    DOWNLOAD_INCOME_CSV: "/api/V1/income/download-excel",
+    ADD_INCOME: "/api/v1/income/add",
+    GET_INCOMES: "/api/v1/income/getAll",
+    DELETE_INCOME: (incomeId) => `/api/v1/income/${incomeId}`,
+    DOWNLOAD_INCOME_CSV: "/api/v1/income/download-excel",
   },
   EXPENSE: {
-    ADD_EXPENSE: "/api/V1/expense/add",
-    GET_EXPENSES: "/api/V1/expense/get",
-    DELETE_EXPENSE: (expenseId) => `/api/V1/expense/${expenseId}`,
-    DOWNLOAD_EXPENSE_CSV: "/api/V1/expense/download-excel",
+    ADD_EXPENSE: "/api/v1/expense/add",
+    GET_EXPENSES: "/api/v1/expense/get",
+    DELETE_EXPENSE: (expenseId) => `/api/v1/expense/${expenseId}`,
+    DOWNLOAD_EXPENSE_CSV: "/api/v1/expense/download-excel",
   },
   IMAGE: {
-    UPLOAD_IMAGE: "/api/V1/image/upload",
+    UPLOAD_IMAGE: "/api/v1/uploads/uploadProfileImage",
   },
 };
