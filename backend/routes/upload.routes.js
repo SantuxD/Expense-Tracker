1e242d25154e0f22d84b11365b2a7dbee1bc08cd
Santu Mondal <mondalsantu104@gmail.com>
2026-01-26 22:21:38 +0530

    modify uploads routes

--- backend/routes/upload.routes.js
+++ backend/routes/upload.routes.js
@@ -1,24 +1,41 @@
 const express = require("express");
-
+const { protect } = require("../middleware/auth.middleware");
+const User = require("../models/User.models");
 const upload = require("../middleware/upload.middleware");
 
 const router = express.Router();
 
-router.post("/uploadProfileImage", upload.single("image"), async (req, res) => {
+router.post(
+  "/uploadProfileImage",
+  protect,
+  upload.single("image"),
+  async (req, res) => {
     try {
-        if (!req.file) {
-            return res.status(400).json({ message: "No file uploaded" });
-        }
-        const profileImageUrl = `${req.protocol}://${req.get("host")}/uploads/${req.file.filename
-            }`;
-        return res
-            .status(200)
-            .json({ message: "File uploaded successfully", profileImageUrl });
+      if (!req.file) {
+        return res.status(400).json({ message: "No file uploaded" });
+      }
+      const profileImageUrl = `${req.protocol}://${req.get("host")}/uploads/${
+        req.file.filename
+      }`;
+
+      const user = await User.findById(req.user.id);
+      user.profileImageUrl = profileImageUrl;
+      await user.save();
+
+      return res
+        .status(200)
+        .json({
+          message: "File uploaded successfully",
+          profileImageUrl,
+          user,
+          token: req.token,
+        });
     } catch (error) {
-        return res
-            .status(500)
-            .json({ message: "Error uploading file: " + error.message });
+      return res
+        .status(500)
+        .json({ message: "Error uploading file: " + error.message });
     }
-});
+  },
+);
 
 module.exports = router;
