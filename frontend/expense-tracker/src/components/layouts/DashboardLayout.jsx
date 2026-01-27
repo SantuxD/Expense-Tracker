a71302b8b6a55b6fda7a0c87fa734d317f3a3541
Santu Mondal <mondalsantu104@gmail.com>
2026-01-27 23:10:31 +0530

    Modify dashboard layout

--- frontend/expense-tracker/src/components/layouts/DashboardLayout.jsx
+++ frontend/expense-tracker/src/components/layouts/DashboardLayout.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Navbar from "./Navbar";
 import SideMenu from "./SideMenu";
-import { UserContext } from "../../context/Usercontext";
+import { UserContext } from "../../context/UserContext";
 
 const DashboardLayout = ({children, activeMenu}) => {
 
