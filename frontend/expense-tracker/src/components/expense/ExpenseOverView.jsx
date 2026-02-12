fa671ef8057b55da276fa5f508d5861f9e800ec6
Santu Mondal <mondalsantu104@gmail.com>
2026-02-12 22:35:45 +0530

    feat(expense): add ExpenseOverview component with line chart visualization

---
+++ frontend/expense-tracker/src/components/expense/ExpenseOverView.jsx
@@ -0,0 +1,36 @@
+import { LuPlus } from "react-icons/lu";
+import CustomBarChart from "../Charts/CustomBarChart";
+import { useEffect, useState } from "react";
+import { prepareIncomeBarChartData } from "../../utils/helper";
+import CustomLineChart from "../Charts/CustomLineChart";
+
+const ExpenseOverView = ({ transactions, onAddExpense }) => {
+  const [chartData, setChartData] = useState([]);
+
+  useEffect(() => {
+    const result = prepareIncomeBarChartData(transactions);
+    setChartData(result);
+  }, [transactions]);
+
+  return (
+    <div className="card">
+      <div className="flex items-center justify-between">
+        <div className="">
+          <h5 className="text-lg">Expense details</h5>
+          <p className="text-xs text-gray-400 mt-0.5">
+            {" "}
+            Track your spendings over time and gain insights your money goes
+          </p>
+        </div>
+        <button className="add-btn" onClick={onAddExpense}>
+          <LuPlus className="text-lg" /> Add Expense
+        </button>
+      </div>
+      <div className="mt-10">
+        <CustomLineChart data={chartData} />
+      </div>
+    </div>
+  );
+};
+
+export default ExpenseOverView;
